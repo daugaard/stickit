9412b125984fea491e7831fe59db86ad0ec382a4
Soren Blond Daugaard <sbd@ineptum.dk>
2013-06-15 12:48:35 -0400

    Load results implemented

--- js/ui_controller.js
+++ js/ui_controller.js
@@ -2,7 +2,22 @@ var UIController = {
   
   load_search_results : function( search_json ){
   
-    alert("Her!");
+    container = $(".results_container").html("");
+    
+    $(search_json.data.results).each( function(index, value)  {
+      //alert(value.domain);
+      result = $(".result-container").clone();
+      $(result).removeClass("result-container");
+      $(result).find(".result-title").html( value.title );
+      $(result).find(".result-description").html( value.description );
+      $(result).find(".result-url").html( value.aggregate_link );
+      $(result).find(".result-url").attr("href", value.aggregate_link );
+
+      container.append( result );
+
+    });
+  
+  
   }
 
 }
