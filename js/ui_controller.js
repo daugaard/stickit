5b6a23bf7331363893071acff68272bb022be0e9
Soren Blond Daugaard <sbd@ineptum.dk>
2013-06-15 14:27:23 -0400

    Added extra stats and summary

--- js/ui_controller.js
+++ js/ui_controller.js
@@ -12,8 +12,24 @@ var UIController = {
       $(result).removeClass("result-container");
       $(result).find(".result-title").html( value.title );
       $(result).find(".result-description").html( value.description );
+      
+      $(result).find(".result-social-score").html( value.score );
+
+      //var d = new Date(value.initial_epoch); // The 0 there is the key, which sets the date to the epoch
+      //$(result).find(".result-first-indexed").html( d.getMonth() + "-" + d.getDate() + "-" + d.getFullYear() );
+      $(result).find(".result-language").html( value.lang );
+      
+      $(result).find(".result-domain").html( value.domain );
+      
+      
+      $(result).find(".result-summary").html( value.content );
+      $(result).find(".result-summary").css("display","none" );
+      
+      
       $(result).find(".result-url").html( value.aggregate_link );
       $(result).find(".result-url").attr("href", value.aggregate_link );
+
+      $(result).addClass("postit");
 
       container.append( result );
 
